a02317e89d6493193a5026917b107ea41028d23e
unknown <mishrachintan@gmail.com>
2025-01-28 02:48:27 +0100

    Added a prompt

--- test_dummy.py
+++ test_dummy.py
@@ -1,1 +1,5 @@
-print("This is a test dummy!")
+print("This is a test dummy!\n")
+
+# Making some modification-1
+x = input("Are you a champion?")
+print("Good!") if x == "yes" else print("Keep going until you become one")
