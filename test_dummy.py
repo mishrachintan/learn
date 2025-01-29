a3a94baa21e1f2f431b0812e5b2edb0acefe710f
CM <mishrachintan@gmail.com>
2025-01-29 01:12:43 +0100

    b1 modified

--- test_dummy.py
+++ test_dummy.py
@@ -3,3 +3,7 @@ print("This is a test dummy!\n")
 # Making some modification-1
 x = input("Are you a champion?")
 print("Good!") if x == "yes" else print("Keep going until you become one")
+
+# Branch-1 modifications
+y = input("Are you consistent? [y/n]")
+print("Nice") if y == "y" else print("Try again today.")
