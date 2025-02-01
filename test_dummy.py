5373d3472fe3c8051ad3881b6e72ab9a33378735
CM <mishrachintan@gmail.com>
2025-02-01 22:03:16 +0100

    After difftool config, commit on main branch

--- test_dummy.py
+++ test_dummy.py
@@ -2,9 +2,13 @@ print("This is a test dummy!\n")
 print("Adding this line in main dated 30Jan2025")
 
 # Making some modification-1
-x = input("Are you a champion?")
-print("Good!") if x == "yes" else print("Keep going until you become one")
+x = input("Are you a champion? [y/n]")
+print("Good!") if x == "y" else print("Keep going until you become one")
 
 # Branch-1 modifications
 y = input("Are you consistent? [y/n]")
 print("Nice") if y == "y" else print("Try again today.")
+
+# Second round of modifications after difftool
+z = input("Did you add difftool to config correctly? [y/n]")
+print("Perfect") if z == "y" else print("Add difftool in <localrep>/.git/confid")
